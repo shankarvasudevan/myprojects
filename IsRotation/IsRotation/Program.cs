bd9ff16e8a3d0bb598fb54f561da97d6ad9f12bf
Shankar Vasudevan <s.vasudevan297@gmail.com>
2016-05-05 10:36:39 +1000

    Checks if one string is a rotation of the other

---
+++ IsRotation/IsRotation/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace IsRotation
+{
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+			string s1 = "anderson";
+			string s2 = "sonander";
+
+			Console.WriteLine(IsRotation(s1,s2));
+		}
+
+		static bool IsRotation(string s1, string s2)
+		{
+			int len = s1.Length;
+
+			if (s2.Length != len)
+			{
+				return false;
+			}
+
+			for (int i=1; i <= len; i++)
+			{
+				string front = s2.Substring(0,i);
+				string back = s2.Substring(i,len-i);
+
+				if (IsSubstring(s1, front) && IsSubstring(s1, back))
+				{
+					return true;
+				}
+
+			}
+
+			return false;
+		}
+
+		static bool IsSubstring(string word, string potentialSubstring)
+		{
+			return word.Contains(potentialSubstring);
+		}
+	}
+}
